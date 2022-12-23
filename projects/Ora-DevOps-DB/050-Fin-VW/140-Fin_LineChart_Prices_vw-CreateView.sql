3c2c10ad91c753a8265ca5d9c0f5ab3058eb7bb2
Mick 277 <Mick277@yandex.com>
2022-12-23 18:14:31 +1000

    Added Fin_LineChart_Prices_vw

--- projects/Ora-DevOps-DB/050-Fin-VW/140-Fin_LineChart_Prices_vw-CreateView.sql
+++ projects/Ora-DevOps-DB/050-Fin-VW/140-Fin_LineChart_Prices_vw-CreateView.sql
@@ -1,31 +1,27 @@
-CREATE OR REPLACE VIEW Fin_LineChart_HistVal_vw AS
+CREATE OR REPLACE VIEW Fin_LineChart_Prices_vw AS
 SELECT
     p.security_code,
     DECODE(s.frequency, 'Y', TO_CHAR(p.price_dt, 'YYYY'), 'M', TO_CHAR(p.price_dt, 'YYYY-DD'),  TO_CHAR(p.price_dt, 'YYYY-MM-DD')) PRICE_DT,
-    p.value,
+    p.close,
     --Params2_pkg.GetNumber('fin_LineChart_Val_Colour') VAL_COLOUR,
-    NULL VAL_COLOUR,
-    NULL LABEL,
+    'LABEL' LABEL,
     'CUSTOM_TOOLTIP'  CUSTOM_TOOLTIP,
     'LEGEND_TOOLTIP' LEGEND_TOOLTIP,
     'LABEL_TOOLTIP' LABEL_TOOLTIP,
     'CUSTOM_COLUMN' CUSTOM_COLUMN
 FROM
     fin_securities s
-    JOIN fin_security_histVal p ON (p.security_code = s.code)
-WHERE s.code = 
+    JOIN fin_security_prices p ON (p.security_code = s.code)
+WHERE archive != -1
 ;
 
+/*
 BEGIN
-    Params2_pkg.SetVarchar2('fin_LineChart_Val_Colour01', '#508223', 'StockChart', 'StockChart bar colour when closing up.');
+    Params2_pkg.SetVarchar2('Fin_Prices_LineChart_Colour01', '#508223', 'StockChart', 'StockChart bar colour when closing up.');
 END;
 /
 
-/* 
+ 
 SELECT * FROM fin_security_hist_Prices_vw;
-
-SELECT code d, code r
-FROM fin_securities
-WHERE archive != -1;
 
 */
